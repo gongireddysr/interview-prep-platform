9063f7eee48aa3924b484cfa55056e7c12caa134
gongireddysr <gongireddysr@gmail.com>
2026-01-14 10:31:17 -0500

    Created System Design UI basic version

--- src/app/prep/page.tsx
+++ src/app/prep/page.tsx
@@ -23,6 +23,8 @@ export default function PrepPage() {
       router.push("/prep/behavioral");
     } else if (selectedCategory === "coding") {
       router.push("/prep/coding");
+    } else if (selectedCategory === "system-design") {
+      router.push("/prep/system-design");
     }
   };
 
